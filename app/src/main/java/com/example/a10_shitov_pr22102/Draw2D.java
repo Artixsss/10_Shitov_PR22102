3b08637d7daa5f6f8b817a1950ccda0676a22288
artixs <Musicartoum@mail.ru>
2024-06-19 22:50:57 +0700

    добавил класс Draw2D

---
+++ app/src/main/java/com/example/a10_shitov_pr22102/Draw2D.java
@@ -0,0 +1,29 @@
+package com.example.a10_shitov_pr22102;
+
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.View;
+
+public class Draw2D extends View {
+
+    private Paint mPaint = new Paint();
+    private Rect mRect = new Rect();
+    private Bitmap mBitmap;
+
+    public Draw2D(Context context) {
+        super(context);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+    }
+}
